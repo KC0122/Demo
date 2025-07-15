55356f187b811722ccc19ff07087c7ad19579b86
KC0122 <sacakameel@yahoo.co.uk>
2025-07-15 22:49:30 +0100

    updates to settings

--- tests/hooks/tearup-teardown.ts
+++ tests/hooks/tearup-teardown.ts
@@ -6,7 +6,7 @@ let page;
 
 test.beforeAll(async function(){
  //Lauch the browser
-  brows = await chromium.launch({headless:false})
+  brows = await chromium.launch({headless:true})
   //Open a new tab it should log in
   contx = await brows.newContext();
   page = await contx.newPage()
